9f322a9074b3bd74d0bc0c7777f3078afdc5192d
frednguyen123 <107221212+frednguyen123@users.noreply.github.com>
2023-08-28 23:49:50 -0700

    git commit

---
+++ 01-CSS Trivia/notes.js
@@ -0,0 +1,81 @@
+/**
+ * CSS TRIVIA QUESTIONS
+ */
+
+/**
+ * Q1: What is the box model?
+ * Box that wraps around every single html element.
+ * Includes margin, border, padding, and content.
+ * Content-Box: Default, width and height only equal to content
+ * Border-Box: Width and Height is equal to content, padding, and border.
+ */
+
+/**
+ * Q2: What is specificty
+ * Specifity is a weight given to a style to determine if it should be
+ * applied or not.
+ * CSS reads specificty ID > Class > HTML element.
+ * CSS Reads the latest element if they are the same specificty.
+ */
+
+/** 
+ * Q3: How to center a div?
+ * 1. Display Flex, Justify Content Center, Align Items Center
+ * 2. Position absolute to its parent, top: 50%, left 50%, then the 
+ *    actual image translate(-50%,-50%).
+ * 3. Display Flex/Grid, Margin Auto;
+ * 
+ */
+
+/** 
+ * Q4: What is the difference between 'div' and 'section'?
+ * Divs are just used to group elements together, doesn't have a specific meaning
+ * Sections are used to group elements that are related to each other
+ * Sections are typically structure with headings then paragraph text.
+ */
+
+/** 
+ * Q5: What is the difference between block, inline, inline-block?
+ * Block: Start on a new line and takes up the whole width
+ * Inline: Start on the same line and you cant set width/height. 
+ * Inline-Block: Starts on the same line, you're allowed to set the width and height
+ */
+
+/** 
+ * Q6: Difference between position relative, absolute, fixed, sticky, and static?
+ * Relative: Flow of page but can use position top, right, left, bottom.
+ * Absolute: Doesn't affect the flow of the page, width must be set
+ * Fixed: When scrolling the position is the same on the entire screen. 
+ * Sticky: Based on scroll location, once we hit scroll position it will go to fixed.
+ * Static: Default, position normal flow of the page, cannot position them.
+ */
+
+/** 
+ * Q7: Difference between visibility: hidden, display: none?
+ * Visibility: keeps the elements in the DOM but makes it invisible on the screen
+ * Display: removes the element completely from the DOM Tree.
+ */
+
+/** 
+ * Q8: How to build a triangle in CSS?
+ * Set width/height to 0 because no content. Entire box is just border
+ * Border-bottom: solid black 100 px, because each side is a triangle.
+ */
+
+/**
+ * Q9: What are pseudo elements in CSS?
+ * Use to give elements special effects ::before ::After (display: inline)
+ * Pseudo Selectors :hover etc.
+ */
+
+/**
+ * Q10: What are data attributes?
+ * Stores data in the DOM. We can use these in our CSS or Javascript
+ */
+
+
+
+
+
+
+
